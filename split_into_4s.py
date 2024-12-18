778e3b6244018fc28d0e8179c30519f4bb7fd089
joyvahini <joyvahini24@gmail.com>
2024-12-17 23:30:52 -0500

    splitting file

--- split_into_4s.py
+++ split_into_4s.py
@@ -1,10 +1,9 @@
 from pydub import AudioSegment
 import os
 
-input_folder_path = "E:/MSc Research/MSc project/github_repo/MSc_Research/datasets/Thodi/Thodi.wav"
-output_folder = 'E:/MSc Research/MSc project/github_repo/MSc_Research/datasets/Thodi/5s'
-
-def split_wav(file_path, output_folder, clip_length=5):
+input_folder_path = "E:/MSc Research/MSc project/github_repo/datasets/Sarasangi/Sarasangi.wav"
+output_folder = 'E:/MSc Research/MSc project/github_repo/datasets/Sarasangi/4s'
+def split_wav(file_path, output_folder, clip_length=4):
     audio = AudioSegment.from_wav(file_path)
 
     total_length = len(audio) // 1000 
@@ -17,14 +16,14 @@ def split_wav(file_path, output_folder, clip_length=5):
         start_time = i * int(clip_length) * 1000
         end_time = start_time + int(clip_length) * 1000
         clip = audio[start_time:end_time]
-        clip_name = os.path.join(output_folder, f'Thodi_{i+1}.wav')
+        clip_name = os.path.join(output_folder, f'Sarasangi_{i+1}.wav')
         clip.export(clip_name, format='wav')
         print(f'Exported {clip_name}')
 
     if total_length % int(clip_length) != 0:
         start_time = num_clips * int(clip_length) * 1000
         clip = audio[start_time:]
-        clip_name = os.path.join(output_folder, f'Thodi_{num_clips+1}.wav')
+        clip_name = os.path.join(output_folder, f'Sarasangi_{num_clips+1}.wav')
         clip.export(clip_name, format='wav')
         print(f'Exported {clip_name}')
 
